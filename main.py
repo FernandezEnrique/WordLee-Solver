89dd7291f1aae4aa9fa847d924ecb01cb1b21776
Enrique Fernandez Velasco <fernandezvelascoenrique@gmail.com>
2023-03-19 00:24:01 +0100

    Can choose languajes
    Added english dictionary, and added a menu to choose languaje

--- main.py
+++ main.py
@@ -1,89 +1,109 @@
 from random import choice
+import os
 
 class Wordlee:
     def __init__(self):
-        self.palabras = []
-        length = int(input("Longitud de la palabra: "))
-        self.load_words(length)
-        while True:
-            print("1)Existe letra y sé su posición\n2)Existe letra pero no sé su posición\n3)No existe\n4)Ver palabras actuales\n5)Exportar a txt\n6)Sugerir palabra")
-            try:
-                option = int(input("Opción: "))
-            except KeyboardInterrupt:
-                exit()
-            except:
-                option = ''
+        self.running = True
+        self.options = [1,2,3,4,5,6,7]
+        self.words = []
+        length = int(input("Length of word: "))
+        
+        self.langs_available = {}
+        for i, lang in enumerate(os.listdir('lang/')):
+            self.langs_available[i] = lang
+        lang_option = self.choose_lang()
+        
+        self.load_words(self.langs_available[lang_option], length)
+
+    def run(self):
+        while self.running:
+            print("1) Exists letter and has a known position")
+            print("2) Exists letter without a known position")
+            print("3) Letter does not exists")
+            print("4) Check remaining words")
+            print("5) Random word")
+            print("6) Exit game")
+            
+            option = input("Select an option: ")
+            while ((not option.isdigit()) or (int(option) not in self.options)):
+                print("Please, enter a valid option")
+                option = input("Select an option: ")
+            
+            option = int(option)
+
             if option == 1:
-                letra = input("Introduce la letra: ")
-                posicion = int(input("Posición: "))
-                self.letter_and_position(letra, posicion)
+                letter = input("Letter: ")
+                posicion = int(input("Position: "))
+                self.letter_and_position(letter, posicion)
                 
             elif option == 2:
-                letras = input("Introduce las letras: ").split(',')
-                self.letter_without_position(letras)
+                letters = input("Letters (leave a blank space between each): ").split(' ')
+                self.letter_without_position(letters)
 
             elif option == 3:
-                letras = input("Introduce las letras: ").split(',')
-                self.no_letter(letras)
+                letters = input("Letters (leave a blank space between each): ").split(' ')
+                self.no_letter(letters)
 
             elif option == 4:
-                print(self.palabras)
-                print(len(self.palabras))
+                print(self.words)
+                print(len(self.words))
 
             elif option == 5:
-                name = input("Nombre del archivo: ")
-                f= open(name,"w+")
-                for palabra in self.palabras:
-                    f.write(palabra+'\n')
-                f.close()
-                print("Palabras exportadas.\n")
-
-            elif option == 6:
-                print(choice(self.palabras))
+                print(choice(self.words))
             
-            elif option == 7:
-                exit()
+            elif option == 6:
+                run = False
+
+    def choose_lang (self):
+        print("\nAvailable languajes")
+        for i in self.langs_available:
+            print(f"{i}) {self.langs_available[i]}")
+
+        option = int(input("Select a languaje: "))
+        print('\n')
+
+        return option
 
     def letter_and_position(self, letra:str, posicion:int):
         palabras_aux = []
-        for i in self.palabras:
+        for i in self.words:
             if letra != str(list(i)[posicion - 1]):
                 palabras_aux.append(i)
         for _palabra in palabras_aux:
-            self.palabras.remove(_palabra)
-        print(f"Palabras restantes: {str(len(self.palabras))}. Palabras eliminadas: {str(len(palabras_aux))}")
+            self.words.remove(_palabra)
+        print(f"Remaining words: {str(len(self.words))}. Discarded words: {str(len(palabras_aux))}")
 
     def letter_without_position(self, letras:list):
         palabras_aux = []
         for letra in letras:
-            for palabra in self.palabras:
+            for palabra in self.words:
                 if letra not in palabra:
                     palabras_aux.append(palabra)
         for _palabra in palabras_aux:
-            self.palabras.remove(_palabra)
-        print(f"Palabras restantes: {str(len(self.palabras))}. Palabras eliminadas: {str(len(palabras_aux))}")
+            self.words.remove(_palabra)
+        print(f"Remaining words: {str(len(self.words))}. Discarded words: {str(len(palabras_aux))}")
 
     def no_letter(self, letras:list):
         palabras_aux = []
         for letra in letras:
-            for palabra in self.palabras:
+            for palabra in self.words:
                 if letra in palabra:
                     palabras_aux.append(palabra)
         for _palabra in palabras_aux:
             try:
-                self.palabras.remove(_palabra)
+                self.words.remove(_palabra)
             except:
                 pass
-        print(f"Palabras restantes: {str(len(self.palabras))}. Palabras eliminadas: {str(len(palabras_aux))}")
+        print(f"Remaining words: {str(len(self.words))}. Discarded words: {str(len(palabras_aux))}")
 
-    def load_words(self, length:int):
-        f = open("palabras.txt", "r")
+    def load_words(self, filename:str, length:int):
+        f = open(f"lang/{filename}", "r")
         for line in f:
             if len(line.strip()) == length:
-                self.palabras.append(line.strip())
+                self.words.append(line.strip())
         f.close()
-        print("Palabras cargadas: "+str(len(self.palabras)))
+        print(f"Loaded words: {str(len(self.words))}")
 
 
 if __name__ == '__main__':
-    Wordlee()
+    Wordlee().run()
